bf65c1ee1cb9485062b46430bf415849d0384e8d
KarthikaSaiKavi <karthika@miis.ae>
2026-05-01 00:14:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import streamlit as st
-st.title("My first streamlit")
-st.write("Hello!Creating a simple web page")
+# Title
+st.title("Student Mark Calculator")
+# Input from user
+name = st.text_input("Enter your name")
+mark1 = st.number_input("Enter Maths mark")
+mark2 = st.number_input("Enter Science mark")
+# Button
+if st.button("Calculate Average"):
+    average = (mark1 + mark2) / 2
+    st.write("Student Name:", name)
+    st.write("Average Mark:", average)
+    # Grade
+    if average >= 90:
+        st.success("Grade A")
+    elif average >= 75:
+        st.success("Grade B")
+    elif average >= 50:
+        st.warning("Grade C")
+    else:
+        st.error("Fail")
